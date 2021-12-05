b4d8f048e6c25494b72d6d4ea52ef4574dcc381d
Ubermensch0608 <melancholyee28@gmail.com>
2021-12-05 13:03:42 +0900

    미니드럼킷-클릭,키보드 이벤트리스너 통합

--- JS/Drum Kit Starting Files/index.js
+++ JS/Drum Kit Starting Files/index.js
@@ -1,52 +1,57 @@
 const heading = document.querySelectorAll(".drum");
 
+// 드럼을 클릭할 때
 for (let i = 0; i < heading.length; i++) {
-  heading[i].addEventListener("click", handleClick);
-
-  function handleClick() {
+  heading[i].addEventListener("click", function () {
     const buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
-      case "w":
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "a":
-        let kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      case "s":
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "d":
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "j":
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "k":
-        let tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "l":
-        let tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      default:
-        console.log(buttonInnerHTML);
-        break;
-    }
-  }
+    handleClick(buttonInnerHTML);
+  });
 }
 
-const audio = new Audio("sounds/tom-1.mp3");
-audio.play();
+// 키보드를 누를 때
+document.addEventListener("keydown", function (evnet) {
+  handleClick(evnet.key);
+});
+
+function handleClick(key) {
+  switch (key) {
+    case "w":
+      const crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "a":
+      const kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "s":
+      const snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      const tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "l":
+      const tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
+}
